aed7b7b0586e2634e361b5b2bc2adb6086269f70
gavinzhou <gavin.zhou@gmail.com>
2020-07-28 18:56:28 +0900

    add output

--- output.tf
+++ output.tf
@@ -1,10 +1,10 @@
-# output "deployment_execution_arn" {
-#   value = aws_api_gateway_deployment._.execution_arn
-# }
+output "deployment_execution_arn" {
+  value = aws_api_gateway_deployment._.execution_arn
+}
 
-# output "rest_api_id" {
-#   value = aws_api_gateway_rest_api._.id
-# }
+output "rest_api_id" {
+  value = aws_api_gateway_rest_api._.id
+}
 
 # output "api_url" {
 #   value = local.api_url
