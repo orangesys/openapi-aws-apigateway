8c014d1419216b20e5a3764bb93b7a1936897e6e
xucheng <jobxucheng@gmail.com>
2020-10-12 15:25:32 +0900

    fixbug

--- main.tf
+++ main.tf
@@ -71,8 +71,8 @@ resource "aws_route53_record" "this" {
   zone_id = var.zone_id
 
   alias {
-    name                   = aws_apigatewayv2_domain_name.this.domain_name_configuration[0].target_domain_name
-    zone_id                = aws_apigatewayv2_domain_name.this.domain_name_configuration[0].hosted_zone_id
+    name                   = var.domain_name
+    zone_id                = var.zone_id
     evaluate_target_health = false
   }
 }
