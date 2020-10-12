793d6993a39aca2d64ce2d6a90487256f6140735
xucheng <jobxucheng@gmail.com>
2020-10-12 15:29:39 +0900

    fixbug

--- main.tf
+++ main.tf
@@ -65,14 +65,13 @@ resource "aws_apigatewayv2_domain_name" "this" {
 }
 
 resource "aws_route53_record" "this" {
-  # count   = var.domain_name != "" ? 1 : 0
-  name    = aws_apigatewayv2_domain_name.this.domain_name
+  name    = aws_apigatewayv2_domain_name.this[count.index].domain_name
   type    = "A"
   zone_id = var.zone_id
 
   alias {
-    name                   = var.domain_name
-    zone_id                = var.zone_id
+    name                   = aws_apigatewayv2_domain_name.this[count.index].domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.this[count.index].domain_name_configuration[0].hosted_zone_id
     evaluate_target_health = false
   }
 }
