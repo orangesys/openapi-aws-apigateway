1f9595e711aff761e33090c5be1b9361ac0f9c5f
xucheng <jobxucheng@gmail.com>
2020-10-13 20:41:54 +0900

    fixbug

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ resource "aws_api_gateway_base_path_mapping" "this" {
   for_each    = var.domain_name != "" ? var.namespace : {}
   api_id      = aws_api_gateway_rest_api._.id
   domain_name = aws_apigatewayv2_domain_name.this[0].id
-  stage       = each.key
+  stage_name  = each.key
 }
 
 resource "aws_route53_record" "this" {
